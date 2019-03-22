a40b9761abd265b6372519d01d2bb242e7edc900
Cris <corgidisco@gmail.com>
2019-03-22 14:46:45 +0900

    add repository create-options

---
+++ src/repository/create-options.ts
@@ -0,0 +1,32 @@
+import { ConstructType, createOptions as createRelaterOptions } from "relater"
+import { RepositoryOptions } from "../interfaces/repository"
+import { metadataIds, metadataEntities, metadataGeneratedValues } from "../metadata"
+
+
+export function createOptions<Entity>(ctor: ConstructType<Entity>): RepositoryOptions<Entity> {
+  const entity = metadataEntities.get(ctor)
+  if (!entity) {
+    throw new Error("not defined entity")
+  }
+  const id = metadataIds.get(ctor)
+  if (!id) {
+    throw new Error("not defined id")
+  }
+  const relaterOptions = createRelaterOptions(ctor)
+  const idColumn = relaterOptions.columns.find((column) => column.property === id.property)
+  if (!idColumn) {
+    throw new Error("not defined id column")
+  }
+  return {
+    name: entity.name,
+    id: {
+      property: id.property,
+      sourceKey: idColumn.sourceKey,
+    },
+    generatedValues: (metadataGeneratedValues.get(ctor) || []).map((value) => ({
+      property: value.property,
+      strategy: value.strategy,
+    })),
+    ...relaterOptions,
+  }
+}
---
+++ test/repository/create-options.test.ts
@@ -0,0 +1,45 @@
+import { createOptions } from "../../src/repository/create-options"
+import { User } from "../stubs/user"
+
+
+describe("testsuite of repository/create-options", () => {
+  it("test createOptions of User", () => {
+    expect(createOptions(User)).toEqual({
+      name: "user",
+      ctor: User,
+      id: {
+        property: "id",
+        sourceKey: "user_id",
+      },
+      generatedValues: [
+        {
+          property: "id",
+          strategy: "uuid",
+        }
+      ],
+      columns: [
+        {
+          property: "id",
+          sourceKey: "user_id",
+          type: "string",
+        },
+        {
+          property: "username",
+          sourceKey: "username",
+          type: "string",
+        },
+        {
+          property: "email",
+          sourceKey: "email",
+          type: "string",
+        },
+        {
+          property: "createdAt",
+          sourceKey: "created_at",
+          type: "string",
+        },
+      ],
+      relations: [],
+    })
+  })
+})
